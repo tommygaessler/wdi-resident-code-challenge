42b213201a51ba0f279e92ab99ce7cd558faf8ef
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-30 21:36:19 -0600

    got data from json file to render to dom

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 (function() {
   'use strict';
 
-  console.log('sanity check');
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    $('#files').on('change', function(event) {
+      var file = event.target.files[0];
+      console.log(file);
+
+      var reader = new FileReader();
+
+      reader.onload = (function(theFile) {
+        console.log(theFile);
+        return function(e) {
+          console.log(e.target.result);
+          JSON.parse(e.target.result).forEach(function(element) {
+            console.log(element);
+            $('#list').append(`<${element.tag}></${element.tag}>`);
+          });
+        };
+      })(file);
+
+      reader.readAsText(file);
+
+    });
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
+
 
 }());
