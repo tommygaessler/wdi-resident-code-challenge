5fdfa20194d10830bfce37ea0da446661018578f
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-31 00:02:13 -0600

    data appears on page in required format

--- main.js
+++ main.js
@@ -4,17 +4,45 @@
   if (window.File && window.FileReader && window.FileList && window.Blob) {
     $('#files').on('change', function(event) {
       var file = event.target.files[0];
-      console.log(file);
+      // console.log(file);
 
       var reader = new FileReader();
 
       reader.onload = (function(theFile) {
-        console.log(theFile);
+        // console.log(theFile);
         return function(e) {
-          console.log(e.target.result);
-          JSON.parse(e.target.result).forEach(function(element) {
-            console.log(element);
-            $('#list').append(`<${element.tag}></${element.tag}>`);
+          // console.log(e.target.result);
+          JSON.parse(e.target.result).forEach(function(parentElement, index) {
+            // console.log(parentElement);
+            $('#list').append(`<${parentElement.tag}></${parentElement.tag}>`);
+
+            if (parentElement.content.length) {
+              // another forEach because its an array
+              // console.log('Array', parentElement.content);
+
+              parentElement.content.forEach(function(childElement) {
+                // console.log(typeof childElement.content);
+
+                if (typeof childElement.content === 'string') {
+                  // console.log('string', childElement.content);
+                  // add 1 to nth child beacuse it is 1 based index not 0
+                  $(`${parentElement.tag}:nth-child(${index+1})`).append(`<${childElement.tag}>${childElement.content}</${childElement.tag}>`);
+
+                } else {
+                  // console.log('array', childElement);
+
+                  $(`${parentElement.tag}:nth-child(${index+1})`).append(`<${childElement.tag}></${childElement.tag}>`);
+
+                  childElement.content.forEach(function(innerElement) {
+                    $(`${childElement.tag}`).append(`<${innerElement.tag}>${innerElement.content}</${innerElement.tag}>`)
+                  });
+                }
+              });
+            } else {
+              // write to dom
+              $(`${parentElement.tag}`)
+              .append(`<${parentElement.content.tag}>${parentElement.content.content}</${parentElement.content.tag}>`);
+            }
           });
         };
       })(file);
